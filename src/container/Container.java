3b7a48e671d423089898101d1f5d30e47124622c
grzesiek <grzesiek@debian>
2017-03-28 19:29:47 +0200

    Create addSorted method

--- src/container/Container.java
+++ src/container/Container.java
@@ -2,60 +2,67 @@ package container;
 
 import java.util.Optional;
 
+interface List<T> {
 
-
-interface Opcje<T> {
     void showAll();
+
     void addOnBack(T n);
+
     void add(T n);
+
     void addSorted(T n);
+
     int size();
+
     T getNode(int n);
-   // T size();
-    //T getNode(T n);
+
+    void removeNode(int n);
+
+    <T> void reverse(Element top);
 }
 
+class Container<T> implements List<T> {
 
-class Container<T> implements Opcje<T>{
-    
-    private Numbers<T> top = null;
-    private Numbers<T> current = null;
-    private Numbers<T> last = null;
-    private Numbers<T> insert = null;
-    private Numbers<T> previous = null;
+    private Element<T> top = null;
+    private Element<T> current = null;
+    private Element<T> last = null;
+    private Element<T> insert = null;
+    private Element<T> previous = null;
     Optional<T> optional = Optional.empty();
-    
 
     @Override
     public void add(T n) {
-    	optional = Optional.ofNullable(n);
-        while(optional.isPresent()) {
-            current = new Numbers<>(n);
-            if(top==null) top = current;
-            else last.next = current;  
+        optional = Optional.ofNullable(n);
+        while (optional.isPresent()) {
+            current = new Element<>(n);
+            if (top == null) {
+                top = current;
+            } else {
+                last.next = current;
+            }
             last = current;
             System.out.println("number " + current.num);
             break;
         }
     }
-    
+
     @Override
     public void addOnBack(T n) {
         optional = Optional.ofNullable(n);
-        if(optional.isPresent()) {
-            
-            current = new Numbers<>(n);
+        if (optional.isPresent()) {
+
+            current = new Element<>(n);
             current.next = top;
-             
+
             top = current;
             System.out.println("number " + top.num);
         }
     }
-    
+
     @Override
     public void showAll() {
         current = top;
-        while(current!=null) {
+        while (current != null) {
             System.out.println(current.num);
             current = current.next;
         }
@@ -63,27 +70,41 @@ class Container<T> implements Opcje<T>{
 
     @Override
     public void addSorted(T n) {
-           
-            insert = new Numbers<>(n);
-            previous = null;
-            current = top;
-            
-            while(current!=null && current.num.toString().compareToIgnoreCase(n.toString())<0) {
-                    previous = current;
-                    current = current.next;
-            }
-          
-                insert.next = current;
-                if(previous==null) top = insert;  
-                else
-                previous.next = insert;
+        int a = 0;
+        int b = 0;
+        insert = new Element<>(n);
+        previous = null;
+        current = top;
+        if (current != null && current.num instanceof Integer) {
+            a = (Integer) n;
+        }
+
+        while (current != null && current.num instanceof Integer
+                && (b = (Integer) current.num) < a) {
+            previous = current;
+            current = current.next;
+        }
+
+        while (current != null
+                && current.num instanceof String
+                && current.num.toString().compareToIgnoreCase(n.toString()) < 0) {
+            previous = current;
+            current = current.next;
+        }
+
+        insert.next = current;
+        if (previous == null) {
+            top = insert;
+        } else {
+            previous.next = insert;
+        }
     }
 
     @Override
     public int size() {
         int size = 0;
         current = top;
-        while(current!=null) {
+        while (current != null) {
             size++;
             current = current.next;
         }
@@ -93,32 +114,71 @@ class Container<T> implements Opcje<T>{
     @Override
     public T getNode(int n) {
         int size = this.size();
-        Numbers<T> searchingNumber = top;
-        Numbers<T> target = null;
+        Element<T> searchingNumber = top;
+        Element<T> target = null;
         T t = null;
-        if(n<=size()&& searchingNumber!=null) {
-            for(int a=0; a<n; a++) {
+        if (n <= size() && searchingNumber != null) {
+            for (int a = 0; a < n; a++) {
                 target = searchingNumber;
                 System.out.println("inside loop " + searchingNumber.num);
-                searchingNumber = searchingNumber.next;      
-            }       
+                searchingNumber = searchingNumber.next;
+            }
+        } else {
+            System.out.println("indexOut");
         }
-        else System.out.println("indexOut");
-        if(target!=null) t = target.num;
-        else System.out.println("null");
-        
+        if (target != null) {
+            t = target.num;
+        } else {
+            System.out.println("null");
+        }
+
         return t;
     }
-    public Numbers getTop() {
+
+    public Element<T> getTop() {
         return this.top;
+    }
+
+    @Override
+    public void removeNode(int n) {
+        if (n <= 0 || n > size()) {
+            System.out.println("indexOut");
+        } else {
+            if (n == 1) {
+                top = top.next;
+            }
+            current = null;
+            current = top;
+
+            if (current != null) {
+                for (int a = 1; a < n; a++) {
+                    previous = current;
+                    current = current.next;
+                }
+            }
+
+            if (n > 1) {
+                previous.next = current.next;
+            }
+        }
+
+    }
+
+    @Override
+    public <T> void reverse(Element top) {
+        if (top != null) {
+            reverse(top.next);
+            System.out.println(top.num);
+        }
     }
 }
 
-class Numbers<T> {
+class Element<T> {
+
     T num;
-    Numbers next;
-    
-    public Numbers(T n) {
+    Element next;
+
+    public Element(T n) {
         num = n;
         next = null;
     }
