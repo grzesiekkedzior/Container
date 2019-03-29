0a96682b5d5e241421772e2ab2e21c3e32522d6a
grzesiek <grzesiekkedzior@gmail.com>
2019-03-29 13:24:27 +0100

    Implement put, size, isEmpty method.

--- src/grzesiek/collection/map/LinkedMap.java
+++ src/grzesiek/collection/map/LinkedMap.java
@@ -1,46 +1,160 @@
 package grzesiek.collection.map;
 
+/**
+ * @author grzesiek
+ *
+ * @param <K>
+ * @param <V>
+ */
 public class LinkedMap<K, V> implements Map<K, V> {
-	
-	private Node<K, V> node = null;
-	
-	
+
+	private Node<K, V> top = null;
+	private Node<K, V> current = null;
+	private Node<K, V> last = null;
+
+	/**
+	 * Default constructor.
+	 */
+	public LinkedMap() {
+	}
+
+	/**
+	 * @param key
+	 * @param value
+	 */
+	public LinkedMap(K key, V value) {
+		top = new Node<>(key, value);
+	}
 
 	@Override
 	public void put(K key, V value) {
-		// TODO Auto-generated method stub
 
+		if (key != null)
+			current = new Node<>(key, value);
+
+		if (checkIffindKey(key, value)) {
+			Node<K, V> findekey = top;
+			while (findekey != null) {
+				if (findekey.key.equals(key))
+					findekey.value = value;
+				else
+					break;
+				findekey = findekey.next;
+
+			}
+
+		} else {
+			while (current != null) {
+				if (top == null) {
+					top = current;
+				} else
+					last.next = current;
+				last = current;
+				break;
+			}
+
+		}
+
+	}
+
+	private boolean checkIffindKey(K key, V value) {
+		Node<K, V> findekey = top;
+		boolean colisionkey = false;
+		while (findekey != null) {
+			if (findekey.key.equals(key)) {
+				colisionkey = true;
+			} else {
+				break;
+			}
+			findekey = findekey.next;
+
+		}
+		return colisionkey;
 	}
 
 	@Override
 	public V get(Object key) {
-		// TODO Auto-generated method stub
+		if (key == null)
+			return (V) new NullPointerException();
+
+		Node<K, V> searchingkey = top;
+
+		while (key != null) {
+			if (searchingkey != null && searchingkey.key.equals(key)) {
+				return searchingkey.value;
+			} else {
+				searchingkey = searchingkey.next;
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public void remove(Object key) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		if (top != null)
+			return false;
+		return true;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		Node<K, V> sizenode = top;
+		int size = 0;
+		while (sizenode != null) {
+			sizenode = sizenode.next;
+			size++;
+		}
+		return size;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((current == null) ? 0 : current.hashCode());
+		result = prime * result + ((last == null) ? 0 : last.hashCode());
+		result = prime * result + ((top == null) ? 0 : top.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof LinkedMap))
+			return false;
+		LinkedMap other = (LinkedMap) obj;
+		if (current == null) {
+			if (other.current != null)
+				return false;
+		} else if (!current.equals(other.current))
+			return false;
+		if (last == null) {
+			if (other.last != null)
+				return false;
+		} else if (!last.equals(other.last))
+			return false;
+		if (top == null) {
+			if (other.top != null)
+				return false;
+		} else if (!top.equals(other.top))
+			return false;
+		return true;
 	}
 
 	public class Node<K, V> {
 		K key;
 		V value;
 		Node<K, V> next;
-		
+
 		public Node(K key, V value) {
 			this.key = key;
 			this.value = value;
---
+++ src/grzesiek/collection/tests/LinkedMapTest.java
@@ -0,0 +1,88 @@
+package grzesiek.collection.tests;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+
+import grzesiek.collection.map.LinkedMap;
+import grzesiek.collection.map.Map;
+
+class LinkedMapTest {
+
+	@Test
+	public void testCeateObject() {
+		Map<String, Integer> linkedmap = new LinkedMap<>(); 
+		assertNotNull(linkedmap);
+	}
+	
+	@Test
+	public void testConstructorWithParam() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>("key", 1);
+		assertNotNull(linkedmap);
+	}
+	
+	@Test
+	public void testConstructorWithStringGetValue() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>("key", 1);
+		assertEquals(1, linkedmap.get("key").intValue());
+	}
+	
+	
+	@Test
+	public void testConstructorWithIntegerGetValue() {
+		Map<Integer, Integer> linkedmap = new LinkedMap<Integer, Integer>(1,1);
+		assertEquals(1,linkedmap.get(1).intValue());
+	}
+	
+	@Test
+	public void testConstructorWithDoubleGetValue() {
+		Map<Double, Double> linkedmap = new LinkedMap<Double, Double>(1.0,1.0);
+		assertEquals(1.0, linkedmap.get(1.0).doubleValue());
+		
+	}
+
+	@Test
+	public void testPutMethod() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>();	
+		linkedmap.put("key2", 2);
+		linkedmap.put("key3", 8);
+		assertEquals(2, linkedmap.get("key2").intValue());
+		assertEquals(8, linkedmap.get("key3").intValue());
+		
+	}
+	
+	@Test
+	public void testPutNullValue() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>();
+		linkedmap.put("key",null);
+		assertEquals(null, linkedmap.get("key"));
+		
+	}
+	
+	@Test
+	public void testSize() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>();
+		linkedmap.put("key1", 10);
+		linkedmap.put("key2", 20);
+		assertEquals(2, linkedmap.size());
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>();
+		assertEquals(true, linkedmap.isEmpty());
+	}
+	
+	
+	@Test
+	public void testTheSameKeyOverride() {
+		Map<String, Integer> linkedmap = new LinkedMap<String, Integer>();
+		linkedmap.put("key1", 10);
+		linkedmap.put("key1", 20);
+		linkedmap.put("key1", 30);
+		assertEquals(30, linkedmap.get("key1").intValue());
+		assertEquals(1, linkedmap.size());
+	}
+}
